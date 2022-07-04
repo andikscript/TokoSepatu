3d7b014bb99fe2648f89ba9cbd58accfd1d226dc
andikscript <andikscript@gmail.com>
2022-07-04 22:11:11 +0700

    add Entity for get user pass

---
+++ src/main/java/com/andikscript/tokosepatu/model/UserGet.java
@@ -0,0 +1,43 @@
+package com.andikscript.tokosepatu.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class UserGet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user", nullable = false)
+    private Integer id;
+
+    @Column(name = "username", nullable = false, length = 256)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 2048)
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
