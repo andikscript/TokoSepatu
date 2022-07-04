b25b7d2ae39383ab49f906b3b59e343305989efe
andikscript <andikscript@gmail.com>
2022-07-04 20:18:32 +0700

    add user model

---
+++ src/main/java/com/andikscript/tokosepatu/model/User.java
@@ -0,0 +1,65 @@
+package com.andikscript.tokosepatu.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user", nullable = false)
+    private Integer id;
+
+    @Column(name = "nama", nullable = false, length = 128)
+    private String nama;
+
+    @Column(name = "email", nullable = false, length = 256)
+    private String email;
+
+    @Column(name = "username", nullable = false, length = 256)
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 2048)
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
