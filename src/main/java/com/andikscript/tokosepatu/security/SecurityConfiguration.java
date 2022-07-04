64e0cff180a9a79cd10c5a2c2f6131c7975e31d2
andikscript <andikscript@gmail.com>
2022-07-04 08:38:50 +0700

    add basic authentucation security

---
+++ src/main/java/com/andikscript/tokosepatu/security/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.andikscript.tokosepatu.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    // memperbolehkan akses tanpa menggunakan username dan password
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // format jika accept semua data brang : /api/brand/
+        return (web) -> web.ignoring().antMatchers("/api/brand/B01", "/api/brand/B02");
+    }
+}
