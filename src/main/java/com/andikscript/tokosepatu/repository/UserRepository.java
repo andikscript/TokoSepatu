5b9108329502c7e6445198a53e172ab48619feda
andikscript <andikscript@gmail.com>
2022-07-04 22:10:20 +0700

    add JpaRepository for user

---
+++ src/main/java/com/andikscript/tokosepatu/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.andikscript.tokosepatu.repository;
+
+import com.andikscript.tokosepatu.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, String> {
+    List<User> findUserByUsername(String username);
+    List<User> findUserByPassword(String password);
+}
