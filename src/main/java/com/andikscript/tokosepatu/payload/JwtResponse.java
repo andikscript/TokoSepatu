d6bae5e13b8788751c89795a2617d06eaa3128bd
andikscript <andikscript@gmail.com>
2022-07-10 08:30:48 +0700

    add object for jwt response

---
+++ src/main/java/com/andikscript/tokosepatu/payload/JwtResponse.java
@@ -0,0 +1,35 @@
+package com.andikscript.tokosepatu.payload;
+
+import com.andikscript.tokosepatu.model.Role;
+
+import java.util.List;
+
+public class JwtResponse {
+    private String accessToken;
+    private String username;
+    private String email;
+    private List roles;
+
+    public JwtResponse(String accessToken, String username, String email, List roles) {
+        this.accessToken = accessToken;
+        this.username = username;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public List getRoles() {
+        return roles;
+    }
+}
