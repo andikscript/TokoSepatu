35b92c3f48df7f1b6d046a015ea6f30633b56458
andikscript <andikscript@gmail.com>
2022-07-10 08:27:24 +0700

    add roles repository

---
+++ src/main/java/com/andikscript/tokosepatu/repository/RolesRepository.java
@@ -0,0 +1,11 @@
+package com.andikscript.tokosepatu.repository;
+
+import com.andikscript.tokosepatu.model.ERole;
+import com.andikscript.tokosepatu.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RolesRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(ERole name);
+}
