10a16f31b7f7c3c930e52562bd7af1ba428794f5
andikscript <andikscript@gmail.com>
2022-07-10 08:33:10 +0700

    add path naming api

--- src/main/java/com/andikscript/tokosepatu/controller/UserController.java
+++ src/main/java/com/andikscript/tokosepatu/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/v1")
 public class UserController {
 
     @Autowired
