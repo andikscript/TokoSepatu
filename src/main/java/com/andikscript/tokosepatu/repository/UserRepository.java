ce77e38cf8bef0c8a598bf629e086fbdbe92bdc6
andikscript <andikscript@gmail.com>
2022-07-10 08:28:16 +0700

    add method for check username and email

--- src/main/java/com/andikscript/tokosepatu/repository/UserRepository.java
+++ src/main/java/com/andikscript/tokosepatu/repository/UserRepository.java
@@ -4,9 +4,12 @@ import com.andikscript.tokosepatu.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     List<User> findUserByUsername(String username);
-
     List<User> findUserByPassword(String username);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
 }
