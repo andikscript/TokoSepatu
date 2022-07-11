6d8951b4baa089cd2d9741c99b0dbe51b1950837
andikscript <andikscript@gmail.com>
2022-07-11 15:23:58 +0700

    make access brands only admins

--- src/main/java/com/andikscript/tokosepatu/controller/adminaccess/BrandController.java
+++ src/main/java/com/andikscript/tokosepatu/controller/adminaccess/BrandController.java
@@ -10,6 +10,7 @@ import com.github.fge.jsonpatch.JsonPatchException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,21 +24,25 @@ public class BrandController {
     private BrandRepository brandRepository;
 
     @GetMapping(value = "/brands", produces = "application/json")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<Brand> getBrandAll() {
         return brandRepository.findAll();
     }
 
     @GetMapping(value = "/brand/{id}", produces = "application/json")
+    @PreAuthorize("hasRole('ADMIN')")
     public Brand getBrand(@PathVariable(value = "id") String id) throws ResourceNotFoundException {
         return brandRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Not Found"));
     }
 
     @PostMapping(value = "/brand", consumes = "application/json", produces = "application/json")
+    @PreAuthorize("hasRole('ADMIN')")
     public Brand addBrand(@RequestBody Brand brand) {
         return brandRepository.save(brand);
     }
 
     @PutMapping(value = "/brand/{id}", consumes = "application/json")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Object> putSiswa(@PathVariable(value = "id") String id, @RequestBody Brand brand) {
         Optional<Brand> getBrand = brandRepository.findById(id);
 
@@ -51,6 +56,7 @@ public class BrandController {
     }
 
     @PatchMapping(value = "/brand/{id}", consumes = "application/json-patch+json")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Brand> patchBrand(@PathVariable(value = "id") String id,
                                             @RequestBody JsonPatch patch) {
         try {
@@ -67,6 +73,7 @@ public class BrandController {
     }
 
     @DeleteMapping(value = "/brand/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity deleteBrand(@PathVariable(value = "id") String id) {
         Optional<Brand> getBrand = brandRepository.findById(id);
 
