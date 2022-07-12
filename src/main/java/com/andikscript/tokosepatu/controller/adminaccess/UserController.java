8cd72b8cc71c2e11fa49c1c504964322284db400
andikscript <andikscript@gmail.com>
2022-07-12 08:22:18 +0700

    add roles admin

--- src/main/java/com/andikscript/tokosepatu/controller/adminaccess/UserController.java
+++ src/main/java/com/andikscript/tokosepatu/controller/adminaccess/UserController.java
@@ -7,6 +7,7 @@ import com.andikscript.tokosepatu.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,11 +20,13 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping(value = "/users", produces = "application/json")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<User> getAllUser() {
         return userRepository.findAll();
     }
 
     @PostMapping(value = "/user", consumes = "application/json")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity addUser(@RequestBody User user) {
         HashingSHA256 hash = new HashingSHA256();
 
@@ -40,6 +43,7 @@ public class UserController {
 
     // cek username dan password
     @GetMapping(value = "/userpass", consumes = "application/json")
+    @PreAuthorize("hasRole('ADMIN')")
     public boolean getUser(@RequestBody UserGet userget) {
         HashingSHA256 hash = new HashingSHA256();
         List<User> username = userRepository.findUserByUsername(userget.getUsername());
--- src/main/java/com/andikscript/tokosepatu/controller/adminaccess/UserRolesController.java
+++ src/main/java/com/andikscript/tokosepatu/controller/adminaccess/UserRolesController.java
@@ -3,6 +3,7 @@ package com.andikscript.tokosepatu.controller.adminaccess;
 import com.andikscript.tokosepatu.model.UserRole;
 import com.andikscript.tokosepatu.repository.UserRolesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class UserRolesController {
     private UserRolesRepository userRolesRepository;
 
     @GetMapping("/userroles")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<UserRole> userRoleList() {
         return userRolesRepository.findAll();
     }
