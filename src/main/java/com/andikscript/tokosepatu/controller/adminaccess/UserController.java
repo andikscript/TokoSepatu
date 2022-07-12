5d8da9876010f894680711b259a81b9871f5d273
andikscript <andikscript@gmail.com>
2022-07-12 17:25:36 +0700

    remove unused api controller

--- src/main/java/com/andikscript/tokosepatu/controller/adminaccess/UserController.java
+++ src/main/java/com/andikscript/tokosepatu/controller/adminaccess/UserController.java
@@ -2,7 +2,7 @@ package com.andikscript.tokosepatu.controller.adminaccess;
 
 import com.andikscript.tokosepatu.config.HashingSHA256;
 import com.andikscript.tokosepatu.model.User;
-import com.andikscript.tokosepatu.model.UserGet;
+import com.andikscript.tokosepatu.payload.UsernamePasswordRequest;
 import com.andikscript.tokosepatu.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -25,36 +25,4 @@ public class UserController {
         return userRepository.findAll();
     }
 
-    @PostMapping(value = "/user", consumes = "application/json")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity addUser(@RequestBody User user) {
-        HashingSHA256 hash = new HashingSHA256();
-
-        if (user.getUsername().equals(null) || user.getPassword().equals(null) ||
-                user.getUsername().equals("") || user.getPassword().equals("")) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
-        }
-
-        String password = hash.hashing(user.getPassword());
-        user.setPassword(password);
-        userRepository.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
-    }
-
-    // cek username dan password
-    @GetMapping(value = "/userpass", consumes = "application/json")
-    @PreAuthorize("hasRole('ADMIN')")
-    public boolean getUser(@RequestBody UserGet userget) {
-        HashingSHA256 hash = new HashingSHA256();
-        List<User> username = userRepository.findUserByUsername(userget.getUsername());
-        if (!username.isEmpty()) {
-            String password = hash.hashing(userget.getPassword());
-            List<User> pass = userRepository.findUserByPassword(password);
-            if (!pass.isEmpty()) {
-                return true;
-            }
-            return false;
-        }
-        return false;
-    }
 }
