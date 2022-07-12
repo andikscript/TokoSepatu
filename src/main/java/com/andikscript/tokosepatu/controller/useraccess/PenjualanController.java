ca7581d004bc5c357b973376500d3dad1042ed17
andikscript <andikscript@gmail.com>
2022-07-12 08:31:23 +0700

    add roles user

--- src/main/java/com/andikscript/tokosepatu/controller/useraccess/PenjualanController.java
+++ src/main/java/com/andikscript/tokosepatu/controller/useraccess/PenjualanController.java
@@ -1,11 +1,12 @@
 package com.andikscript.tokosepatu.controller.useraccess;
 
-import com.andikscript.tokosepatu.model.Brand;
+import com.andikscript.tokosepatu.exception.ResourceNotFoundException;
 import com.andikscript.tokosepatu.model.Penjualan;
 import com.andikscript.tokosepatu.repository.PenjualanRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,12 +24,20 @@ public class PenjualanController {
         return penjualanRepository.findAll();
     }
 
+    @GetMapping(value = "/sell/{id}", produces = "application/json")
+    @PreAuthorize("hasRole('USER')")
+    public Penjualan getPenjualan(@PathVariable(value = "id") Integer id) throws ResourceNotFoundException {
+        return penjualanRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Not Found"));
+    }
+
     @PostMapping(value = "/sell", consumes = "application/json", produces = "application/json")
+    @PreAuthorize("hasRole('USER')")
     public Penjualan addPenjualan(@RequestBody Penjualan penjualan) {
         return penjualanRepository.save(penjualan);
     }
 
     @PutMapping(value = "/sell/{id}", consumes = "application/json")
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<Object> putPenjualan(@PathVariable(value = "id") Integer id, @RequestBody Penjualan penjualan) {
         Optional<Penjualan> getPenjualan = penjualanRepository.findById(id);
 
@@ -39,5 +48,18 @@ public class PenjualanController {
         penjualan.setId(id);
         penjualanRepository.save(penjualan);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @DeleteMapping(value = "/sell/{id}")
+    @PreAuthorize("hasRole('USER')")
+    public ResponseEntity deletePenjualan(@PathVariable(value = "id") Integer id) {
+        Optional<Penjualan> getPenjualan = penjualanRepository.findById(id);
+
+        if (!getPenjualan.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        penjualanRepository.deleteById(id);
+        return ResponseEntity.ok().build();
     }
 }
